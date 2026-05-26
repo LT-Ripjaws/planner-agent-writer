40401ca3df8bee9b05be88f638a1c8db0a7a12c9
LT-Ripjaws <nightangle676x@gmail.com>
2026-05-26 17:08:15 +0600

    lru cache

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
@@ -7,4 +9,9 @@ class Settings(BaseSettings):
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-settings = Settings()
+
+@lru_cache # makes sure we create the settings object only once.
+def get_settings() -> Settings:
+    return Settings()
+
+settings = get_settings()
